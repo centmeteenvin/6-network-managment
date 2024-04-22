644a45cc7ed7db8af19611dce697d356f5da5fe6
vincent verbergt <vincent.verbergt@gmail.com>
2024-04-22 11:26:25 +0200

    Created basic command structure

---
+++ command.py
@@ -0,0 +1,84 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from .logging import logger
+import shlex
+import subprocess
+import threading
+
+
+class Command(ABC, threading.Thread):
+    """
+    To execute a command, create a Command object and it will run from creation.
+    A Thread object will be associated with the process.
+    It will log to DEBUG by default.
+    The output is accessible from output property.
+    This class holds a list of all commands that were executed, using the revert static method we can undo all changes.
+    """
+    executedCommands: list[Command] = []
+ 
+ 
+    def __init__(self, command: str, undoCommands : list[str] = None) -> None:
+        ABC.__init__()
+        threading.Thread.__init__(self)
+        args = shlex.split(command)
+        logger.info(f'Executing {args}')
+        self.process = subprocess.Popen(
+            args, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+        self.output = []
+        self.undoCommands = undoCommands
+        self._loggingLevel = 'INFO'
+        Command.executedCommands.append(self)
+        self.start()
+
+    def readOutput(self) -> None:
+        for line in self.process.stdout.readlines():
+            processedLine = line.decode().strip()
+            logger.log(self._loggingLevel ,processedLine)
+            self.output.append(processedLine)
+    
+    @abstractmethod
+    def _post_init(self) -> None:
+        """Code to execute after the process has starter in the main thread"""
+        pass
+    
+    @abstractmethod
+    def _run(self) -> None:
+        """Overwrite to execute additional behavior every loop"""
+        pass
+    
+    def run(self) -> None:
+        while True:
+            self.readOutput()
+            
+            if self.process.poll() is not None:
+                return # The process has finished so should this thread.
+    
+    def undo(self) -> None:
+        """This command is run when the script needs to undo all of it's work."""
+        for command in self.undoCommands:
+            UndoCommand(command)
+            
+class BlockingCommand(Command):
+    """By its very nature all commands should be blocking, consider using this only for processes where the session can't be closed"""
+    def _post_init(self) -> None:
+        self.process.wait()
+    
+    def _run(self) -> None:
+        pass
+
+class UndoCommand(BlockingCommand):
+    """These commands are the undo's of other commands and cannot be undone themselves"""
+    def undo(self) -> None:
+        #Does nothing and should not be called.
+        pass
+    
+class BackgroundCommand(Command):
+    def __init__(self, command: str, undoCommands: list[str] = None, loggingLevel = 'DEBUG') -> None:
+        super().__init__(command, undoCommands)
+        self._loggingLevel = loggingLevel
+    
+    def _post_init(self) -> None:
+        pass
+    
+    def _run(self) -> None:
+        pass
