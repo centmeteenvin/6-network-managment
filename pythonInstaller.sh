6455581fced359b356587569f9eb21b0aa85097f
vincent verbergt <vincent.verbergt@gmail.com>
2024-04-25 09:36:53 +0200

    Added a python installer

---
+++ pythonInstaller.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.10 python3.10-venv python3.10-dev
+sudo chmod +x pythonInstaller.sh
+sudo chmod +x update.sh
